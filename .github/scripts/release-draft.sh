714fbe5fb3105e1bed42d898f88e52c6b9bdb595
MichalKalke <michalkalke@gmail.com>
2024-09-01 10:40:39 +0200

    Local test

--- .github/scripts/release-draft.sh
+++ .github/scripts/release-draft.sh
@@ -10,7 +10,7 @@ set -o pipefail # prevents errors in a pipeline from being masked
 
 RELEASE_TAG=$1
 
-REPOSITORY=${REPOSITORY:-kyma-project/warden}
+REPOSITORY=${REPOSITORY:-MichalKalke/warden}
 GITHUB_URL=https://api.github.com/repos/${REPOSITORY}
 GITHUB_AUTH_HEADER="Authorization: Bearer ${GITHUB_TOKEN}"
 
